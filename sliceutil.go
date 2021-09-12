e9068b7cabe1c7034304e8f9162e464c37a64ff2
Douglas <douglas.secco@gmail.com>
2021-09-12 16:35:21 -0300

    a

--- sliceutil.go
+++ sliceutil.go
@@ -51,7 +51,7 @@ func sliceDifference(slice1 []string, slice2 []string) []string {
 
 
 // merge 2 slices of strings
-func Merge(x, y []string]) interface{} {
+func Merge(x []string, y []string]) []string] {
 	return append(x, y)
 }
 
